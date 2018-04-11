46c49bc5c853249874c0fe046fffc15f5652fec8
YS <yuansaii@qq.com>
2018-04-11 22:45:16 +0800

    传感器接入界面修改

--- src/routes/SensorAccessPage.js
+++ src/routes/SensorAccessPage.js
@@ -7,62 +7,57 @@ const { Header, Sider, Content, Footer } = Layout;
 const { Column } = Table;
 const data = [{
   key: '1',
-  sensorID: 'sensorID1',
-  sensorName: 'sensorName1',
+  sensorID: 'urn:liesmars:insitusensor:platform:BaoxieWeatherSoilStation1',
+  sensorName: '豹澥气象土壤监测站1',
   status: 'active',
-  uptime: '10s',
 }, {
   key: '2',
-  sensorID: 'sensorID1',
-  sensorName: 'sensorName1',
+  sensorID: 'urn:liesmars:insitusensor:platform:BaoxieWeatherSoilStation2',
+  sensorName: '豹澥气象土壤监测站2',
   status: 'active',
-  uptime: '10s',
 }, {
   key: '3',
-  sensorID: 'sensorID1',
-  sensorName: 'sensorName1',
+  sensorID: 'urn:liesmars:insitusensor:platform:BaoxieHydrologicalStation',
+  sensorName: '豹澥水文监测站',
   status: 'active',
-  uptime: '10s',
 }];
 export default class UserCenterPage extends React.Component {
   state = {
     collapsed: false,
-    username:null,
+    username: null,
   };
 
   componentDidMount() {
     // this.runPolling();
-    //用于获取session中用户信息，若存在则获取到用户名称
+    // 用于获取session中用户信息，若存在则获取到用户名称
 
     fetch('http://www.myflood.com/getCurrentUser', {
       method: 'POST',
       credentials: 'include',
       headers:
-        {
-          Accept: 'application/json',
-          'Content-Type': 'application/json; charset=utf-8',
-        },
-    }).then((response) => {
-        if (response.status >= 200 && response.status < 300) {
-          return response.json();
-        } else {
-          const error = new Error(response.statusText);
-          error.response = response;
-          throw error;
-        }
+      {
+        Accept: 'application/json',
+        'Content-Type': 'application/json; charset=utf-8',
       },
+    }).then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      } else {
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      }
+    },
     ).then((data) => {
       if (data.flag) {
-        //当前是否登录
+        // 当前是否登录
         this.setState({
-          username:data.object,
+          username: data.object,
         });
       }
     }).catch((error) => {
       alert(error.toString());
     });
-
-
   }
   handleMenuClick=(e) => {
     const key = e.key;
@@ -113,11 +108,11 @@ export default class UserCenterPage extends React.Component {
             <Menu.Item key="1"><Link to="/access">传感器观测接入</Link></Menu.Item>
             <Menu.Item key="2"><Link to={path}>洪涝事件订阅</Link></Menu.Item>
             <Menu.Item key="3"><Link to="/manage">洪涝事件管理</Link></Menu.Item>
-            <Menu.Item key="4" style={{ float: 'right' }}>{this.state.username==null?<Link to="/login">登录</Link>:`尊敬的${this.state.username}，您好！`}</Menu.Item>
+            <Menu.Item key="4" style={{ float: 'right' }}>{this.state.username == null ? <Link to="/login">登录</Link> : `尊敬的${this.state.username}，您好！`}</Menu.Item>
           </Menu>
         </Header>
         <Layout>
-          <Content style={{ margin: '12px 0px 0px 12px', padding: 0, background: '#fff', minHeight: 1000  }}>
+          <Content style={{ margin: '12px 0px 0px 12px', padding: 0, background: '#fff', minHeight: 1000 }}>
             {/* 此处存放现在已经接入的传感器设置，管理传感器数据的接入*/}
             <br />
             请选择上传的传感器拓扑：<Button type={'primary'}>上传传感器接入拓扑</Button>
@@ -142,17 +137,11 @@ export default class UserCenterPage extends React.Component {
                 key="sensorName"
               />
               <Column
-                title="上传时间"
-                dataIndex="uptime"
-                key="uptime"
-              />
-              <Column
                 title="状态"
                 dataIndex="status"
                 key="status"
               />
               <Column title="操作" render={this.operator} />
-              <Column title="在地图上查看" render={this.operator}/>
             </Table>
           </Content>
         </Layout>
